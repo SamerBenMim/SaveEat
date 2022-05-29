d63339e8198c92ee63370823aeaccd2c8e6831eb
SamerBenMim <samerbenmim@insat.u-carthage.tn>
2022-05-29 22:26:43 +0100

    cart&redux

--- models/boxModel.js
+++ models/boxModel.js
@@ -5,17 +5,26 @@ const boxSchema = new mongoose.Schema({
         type: String,
         required: [true, "please provide a category"],
         lowercase: true,
+        default:"mixed",
+        enum: ['mixed', 'canned','fresh'],
     },
 
     subCategory: {
         type: String,
         required: [true, "please provide a subcategory"],
         lowercase: true,
+        default:"regular",
+        enum: ['special', 'regular'],
+
     },
 
     price: {
         type: Number,
         default: 0,
+    },
+    stock:{
+        type: Number,
+        default: 1,
     },
 
     items: { type : [mongoose.Schema.ObjectId], ref: 'Item' },
