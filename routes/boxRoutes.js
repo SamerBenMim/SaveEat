27c93fc2b2bcc1b9117ecbdaf1d774b08f492857
SamerBenMim <samerbenmim@insat.u-carthage.tn>
2022-05-30 19:59:49 +0100

    admin auth

--- routes/boxRoutes.js
+++ routes/boxRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router();
+const { isAdmin } = require('../Middleware/isAdmin')
 
 const { getAllboxes,addBox,removeBox,updateBox} = require("../controllers/boxController")
  router.patch('/updateBox/:id', updateBox)
